335a169c17735381ee6f9014554a4fee3e18d4ca
Omri Levy <omrilevy888@gmail.com>
2022-05-09 20:43:29 +0300

    #41 A very basic icon

--- lib/main.dart
+++ lib/main.dart
@@ -40,8 +40,8 @@ class _SongTinderAppState extends State<SongTinderApp> {
               brightness: Brightness.light,
               primary: Color.fromARGB(255, 255, 255, 255),
               onPrimary: Color.fromARGB(255, 36, 36, 36),
-              secondary: Colors.red,
-              onSecondary: Colors.yellow,
+              secondary: Color.fromARGB(255, 241, 9, 9),
+              onSecondary: Color.fromARGB(255, 255, 255, 255),
               error: Colors.red,
               onError: Colors.white,
               background: Colors.green,
@@ -76,8 +76,8 @@ class _SongTinderAppState extends State<SongTinderApp> {
               brightness: Brightness.dark,
               primary: Color.fromARGB(255, 36, 36, 36),
               onPrimary: Color.fromARGB(255, 255, 255, 255),
-              secondary: Colors.red,
-              onSecondary: Colors.yellow,
+              secondary: Color.fromARGB(255, 255, 255, 255),
+              onSecondary: Color.fromARGB(255, 241, 9, 9),
               error: Colors.red,
               onError: Colors.white,
               background: Colors.green,
@@ -119,10 +119,21 @@ class AppBody extends StatelessWidget {
         length: 2,
         child: Scaffold(
             appBar: AppBar(
-              title: Text(
-                'Song Tinder',
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
+              title: Stack(alignment: Alignment.center, children: [
+                Icon(
+                  Icons.favorite,
+                  size: MediaQuery.of(context).size.width / 6,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                Transform.rotate(
+                  angle: 0.1,
+                  child: Icon(
+                    Icons.music_note_sharp,
+                    size: MediaQuery.of(context).size.width / 12,
+                    color: Theme.of(context).colorScheme.onSecondary,
+                  ),
+                ),
+              ]),
               actions: const <Widget>[ThemeButton(), SettingsButton()],
               backgroundColor: Theme.of(context).colorScheme.primary,
             ),
